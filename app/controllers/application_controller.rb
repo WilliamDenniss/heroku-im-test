ba011b028d42c33f92c713e15427670d67b96f58
William Denniss <will@icog.net>
2013-03-19 11:13:59 -0700

    requiring Magick

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
+require 'RMagick'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include Magick
 
   def magick
     render :inline => "" + Magick::Magick_version
